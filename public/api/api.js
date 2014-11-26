8a5856122ccab6f9663a4c131de2a6c25926278a
Gojko Adzic <gojko@gojko.com>
2014-11-26 13:48:19 +0100

    initial api docs

---
+++ public/api/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ActivityLog",
+        "GoldApi",
+        "GoldLicenseManager",
+        "JsonStorage",
+        "LayoutExportController",
+        "S3Api"
+    ],
+    "modules": [
+        "MM"
+    ],
+    "allModules": [
+        {
+            "displayName": "MM",
+            "name": "MM",
+            "description": "MindMup API"
+        }
+    ]
+} };
+});
